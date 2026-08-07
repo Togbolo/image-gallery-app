fae10235526ecdbee10d422681d9363cf3aad092
togbolo <87999565+Togbolo@users.noreply.github.com>
2026-08-07 12:19:35 +0000

    Add files via upload

---
+++ src/HeaderComponent.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+     <div className="header">
+    <h1 className="header__title">HIGH MORAL INTERNATIONAL SCHOOL</h1>
+    <p className="header__description">
+      Graduation Ceremony Image Gallery
+    </p>
+    
+  </div>
+  )
+}
+
+export default HeaderComponent
